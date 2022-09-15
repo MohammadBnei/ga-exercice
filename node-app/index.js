5df3a19be7d2a1cf99960aa11c9dc6af009e8717
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-09-15 20:50:19 +0200

    testing self-hosted runner

--- node-app/index.js
+++ node-app/index.js
@@ -4,4 +4,23 @@ function sum(a, b) {
   return a + b;
 }
 
+const osu = require("node-os-utils");
+const cpu = osu.cpu;
+
+cpu.usage().then((cpuPercentage) => {
+  console.log(cpuPercentage); // 10.38
+});
+
+var osCmd = osu.osCmd;
+
+osCmd.whoami().then((userName) => {
+  console.log(userName); // admin
+});
+
+const drive = osu.drive;
+
+drive.info().then((info) => {
+  console.log(info);
+});
+
 module.exports = sum;
